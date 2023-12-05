2efa065c2a92247edbdbb6ab94e8d07defa1cb0e
Zombie <adiya@ureca.com>
2023-12-05 09:04:23 +0800

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/flowbite-react/**/*.js",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        gradient:
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%, linear-gradient(153deg, #370617 0.42%, #370617 0.43%, #6A040F 51.34%, #9D0208 101.22%))"
       },
-    },
+      screens: {
+        sm: "330px",
+        md: "768px"
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")]
+};
